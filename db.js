21ee82f75e1e0117e3c9573afa145708c87b86c1
Caio Gonçalves <caiogsan@hotmail.com>
2023-10-20 17:44:09 -0300

    teste

--- db.js
+++ db.js
@@ -1,8 +1,23 @@
-import 'dotenv/config';
-import postgres from 'postgres';
+const postgres = require('postgres');
+require('dotenv').config();
 
-const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
 
-const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${ENDPOINT_ID}`;
+const sql = postgres({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: 'require',
+  connection: {
+    options: `project=${ENDPOINT_ID}`,
+  },
+});
 
-export const sql = postgres(URL, {ssl: 'require'})
+async function getPgVersion() {
+  const result = await sql`select version()`;
+  console.log(result);
+}
+
+getPgVersion();
